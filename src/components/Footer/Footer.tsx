7d305c1f7f189f98ca3c348eb70878ac2de62d5c
alekseibaranov <baranov.sys@gmail.com>
2021-02-10 10:59:18 +0300

    Updates

--- src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Footer(){
-    return (
-        <footer>
-            <h3>FOOTER FOOTER FOOTER</h3>
-        </footer>
-    )
-}
--- src/components/common/Preloader/Preloader.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import s from './preloader.module.sass'
-import preloader from '../../../img/preloader.gif'
-
-export const Preloader = ( ) => {
-	return <div className={s.preloader}>
-		<img src={preloader} alt={'descriptions'}/>
-	</div>
-}
