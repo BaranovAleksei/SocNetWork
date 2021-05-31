e09ceacc4aa0d154650bff3b51946e443c305ad0
alekseibaranov <baranov.sys@gmail.com>
2021-05-31 12:22:08 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SocNetWork/precache-manifest.02c1d14b1700ebf3dc2e99e3a4c940f6.js"
+  "/SocNetWork/precache-manifest.51e8d9494bdb3422ed5120c2772713bf.js"
 );
 
 self.addEventListener('message', (event) => {
